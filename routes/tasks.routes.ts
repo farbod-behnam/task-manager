2e973d1bf32f45f47956b47b2a9b592a880ddfbc
farbod-behnam <farbod.behnam@gmail.com>
2023-02-08 18:02:26 +0330

    add tasks routes

---
+++ routes/tasks.routes.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+import Container from "typedi";
+
+const tasksRouter = express.Router();
+
+tasksRouter.get("/", );
+
+
+export default tasksRouter;
--- server.ts
+++ server.ts
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+import 'reflect-metadata';
+
+import express, { Request, Response } from "express";
+import taskRouter from "./routes/tasks.routes";
+
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/v1/tasks", taskRouter);
+
+server.get("/hello", (req: Request, res: Response) => {
+    res.send("Task Manager App");
+})
+
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log("server is listening on port " + port + "....");
+});
+
